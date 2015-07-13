48c10518bdb5dcd334791a0619a5c2025ce8641f
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-07-13 18:44:14 -0300

    Added new begin solution for AzureDeployAndDebug demo (WIP) This should be the final version of the Chatroom solution

---
+++ Presentation/AzureDeployAndDebug/AzureDeployAndDebug/source/Begin/Chatroom/socketio.js
@@ -0,0 +1,38 @@
+﻿
+
+
+module.exports = function (io) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        
+        //mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+        //    if (err) {
+        //        console.warn(err.message);
+        //    } else {
+        //        var collection = db.collection('chat messages')
+        //        var stream = collection.find().sort().limit(10).stream();
+        //        stream.on('data', function (chat) { console.log('emitting chat'); socket.emit('chat', chat.content); });
+        //    }
+        //});
+        
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+        
+        socket.on('chat', function (msg) {
+            //mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+            //    if (err) {
+            //        console.warn(err.message);
+            //    } else {
+            //        var collection = db.collection('chat messages');
+            //        collection.insert({ content: msg }, function (err, o) {
+            //            if (err) { console.warn(err.message); }
+            //            else { console.log("chat message inserted into db: " + msg); }
+            //        });
+            //    }
+            //});
+            
+            socket.broadcast.emit('chat', msg);
+        });
+    });
+};
