1e978fa46264880f55adc438f968c74c588a05bd
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-07-14 19:02:38 -0300

    Updated final version of Chatroom to use DocumentDB instead of MongoDb

--- Presentation/AzureDeployAndDebug/AzureDeployAndDebug/source/Begin/Chatroom/socketio.js
+++ Presentation/AzureDeployAndDebug/AzureDeployAndDebug/source/Begin/Chatroom/socketio.js
@@ -1,36 +1,47 @@
-﻿
+﻿var DocumentDBClient = require('documentdb').DocumentClient;
+var docdbUtils = require('./docdbUtils');
 
-
-module.exports = function (io) {
+module.exports = function (io, config) {
     io.on('connection', function (socket) {
         console.log('a user connected');
         
-        //mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
-        //    if (err) {
-        //        console.warn(err.message);
-        //    } else {
-        //        var collection = db.collection('chat messages')
-        //        var stream = collection.find().sort().limit(10).stream();
-        //        stream.on('data', function (chat) { console.log('emitting chat'); socket.emit('chat', chat.content); });
-        //    }
-        //});
+        var docDbClient = new DocumentDBClient(process.env.DOCUMENT_DB_HOST, {
+            masterKey: process.env.DOCUMENT_DB_AUTH_KEY
+        });
+        
+        docdbUtils.initCollection(docDbClient, 'chatroom', 'messages', function (err, collection) {
+            if (err) {
+                console.warn(err.message);
+            } else {
+                docDbClient.queryDocuments(collection._self, 'SELECT r.content FROM root r')
+                    .forEach(function (err, msg) {
+                        if (msg) {
+                            console.log('emitting chat');
+                            socket.emit('chat', msg.content);
+                        }
+                    });
+            }
+        });
         
         socket.on('disconnect', function () {
             console.log('user disconnected');
         });
         
         socket.on('chat', function (msg) {
-            //mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
-            //    if (err) {
-            //        console.warn(err.message);
-            //    } else {
-            //        var collection = db.collection('chat messages');
-            //        collection.insert({ content: msg }, function (err, o) {
-            //            if (err) { console.warn(err.message); }
-            //            else { console.log("chat message inserted into db: " + msg); }
-            //        });
-            //    }
-            //});
+            docdbUtils.initCollection(docDbClient, 'chatroom', 'messages', function (err, collection) {
+                if (err) {
+                    console.warn(err.message);
+                } else {
+                    var documentDefinition = { content: msg };
+                    docDbClient.createDocument(collection._self, documentDefinition, function (err, o) {
+                        if (err) {
+                            console.warn(err.message);
+                        } else {
+                            console.log("chat message inserted into db: " + msg);
+                        }
+                    });
+                }
+            });
             
             socket.broadcast.emit('chat', msg);
         });
