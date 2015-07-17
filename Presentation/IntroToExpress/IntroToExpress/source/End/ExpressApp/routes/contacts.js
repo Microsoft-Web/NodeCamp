e7d422f0f3d8969ef54b13b50b81e378b38c3274
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-07-17 19:18:38 -0300

    Minor updates in end solution of IntroToExpress demo

--- Presentation/IntroToExpress/IntroToExpress/source/End/ExpressApp/routes/contacts.js
+++ Presentation/IntroToExpress/IntroToExpress/source/End/ExpressApp/routes/contacts.js
@@ -1,7 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-/* GET contacts. */
+/* GET contacts */
 router.get('/', function (req, res) {
     var contacts = [{ "name": "Jane Doe", "phone": "888-555-1212" }, { "name": "Justin Doe", "phone": "877-123-1212" }];
     res.json(contacts);
